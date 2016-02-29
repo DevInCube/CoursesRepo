6c9bbc903f2ad2cf6ae0173c465ddb30a6493d98
PublicHadyniak <public.hadyniak@gmail.com>
2016-02-29 02:01:20 +0200

    Update main.c

--- courses/prog_base_2/tasks/module/basic/main.c
+++ courses/prog_base_2/tasks/module/basic/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "stack.h"
+
 int main(void) {
-    // in development
+    stack_t * st1 = stack_new();
+    printf("Stack size is %i\n", stack_getCount(st1));
+    int pushVal = 13;
+    printf("Do push %i\n", pushVal);
+    stack_push(st1, pushVal);
+    printf("Stack size is %i\n", stack_getCount(st1));
+    printf("Do pop top value\n");
+    int popVal = stack_pop(st1);
+    printf("Stack pop value is %i\n", popVal);
+    printf("Stack size is %i\n", stack_getCount(st1));
+    stack_free(st1);
     return 0;
 }
