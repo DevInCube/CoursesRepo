76c896713dcb403ffb9926860cb6e37754f808bf
DevInCube <ruslan.hadyniak@gmail.com>
2016-03-12 00:17:34 +0200

    linux version

--- courses/prog_base_2/practice/threads/module/threadapp/main.c
+++ courses/prog_base_2/practice/threads/module/threadapp/main.c
@@ -4,21 +4,15 @@
 #ifdef _WIN32
     #include <windows.h>
 #elif defined __linux__
-
+    #include <unistd.h>
 #endif // _WIN32
 
 #include "thread.h"
 #include "mutex.h"
 
-void _sleep(int ms) {
-#ifdef _WIN32
-    Sleep(ms);
-#elif defined __linux__
+void _sleep(int ms);
 
-#endif // _WIN32
-}
-
-mutex_t * mu;
+static mutex_t * mu;
 
 void * run1(void * args) {
     mutex_lock(mu);
@@ -35,7 +29,7 @@ void * run1(void * args) {
 
 void * run2(void * args) {
     mutex_lock(mu);
-    for(int i = 0; i < 100; i++) {
+    for(int i = 0; i < 50; i++) {
         printf(" %s \n", (const char *)args);
         _sleep(60);
     }
@@ -52,9 +46,17 @@ int main(void) {
     void * res1 = NULL;
     thread_join_result(t1, &res1);
     printf("\nThread 1 res: <<<%s>>>\n", (const char *)(res1));
-    thread_terminate(t2);
+    //thread_terminate(t2);
     _sleep(1000);
     thread_join_result(t2, &res1);
     printf("\nThread 2 res: <<<%s>>>\n", (const char *)(res1));
     return 0;
+}
+
+void _sleep(int ms) {
+#ifdef _WIN32
+    Sleep(ms);
+#elif defined __linux__
+    usleep(ms * 1000);
+#endif // _WIN32
 }
