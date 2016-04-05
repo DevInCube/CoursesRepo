3b84dfe58e0ac29a49b8161e448f63095253b5de
PublicHadyniak <public.hadyniak@gmail.com>
2016-04-05 21:09:12 +0300

    Create socket.h

---
+++ courses/prog_base_2/practice/client_server/libsocket/socket.h
@@ -0,0 +1,22 @@
+#ifndef socket_H_INCLUDED
+#define socket_H_INCLUDED
+
+#define CONNECT_OK 0
+
+typedef struct socket_s socket_t;
+
+socket_t * socket_new(void);
+void socket_free(socket_t * self);
+
+const char * socket_getIPAddress(socket_t * self);
+int socket_getPortNumber(socket_t * self);
+
+int socket_bind(socket_t * socket, int portNumber);
+int socket_listen(socket_t * socket);
+socket_t * socket_accept(socket_t * socket);
+int socket_connect(socket_t * sock, const char * ipaddress, int portNumber);
+int socket_read(socket_t * sock, char * recvBuff, int recvSize);
+int socket_write(socket_t * conn, const char * msg);
+void socket_close(socket_t * conn);
+
+#endif // socket_H_INCLUDED
