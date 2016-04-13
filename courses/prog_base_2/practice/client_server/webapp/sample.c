039a003526964c716fcd045ca8dc04519eea77bf
PublicHadyniak <public.hadyniak@gmail.com>
2016-04-13 14:17:20 +0300

    Update sample.c

--- courses/prog_base_2/practice/client_server/webapp/sample.c
+++ courses/prog_base_2/practice/client_server/webapp/sample.c
@@ -1,3 +1,7 @@
+/*
+	unit-tests for all modules with CMocka
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,15 +13,29 @@ typedef struct request_s request_t;
 typedef struct response_s response_t;
 typedef response_t * (*request_handler_fn)(app_t * app, request_t * req, void * ctx);
 
+/* static files support ? url_for('static', filename='style.css') */
 app_t * app_new(void);
 void app_bind(app_t * self, int port);
 void app_setContext(app_t * self, void * ctx);
 void app_on(app_t * self, const char * uri, request_handler_fn handler, const char * methods[]);
 void app_run(app_t * self); 
+/* void app_run_ip(app_t * self, const char * ipAddr); */
 void app_free(app_t * self); 
 
 response_t * response_new(void);
 void response_free(response_t * self);
+/* void response_setStatus(response_t * self, int statusCode); */
+/* void response_setHeader(response_t * self, const char * key, const char * value); */
+/* void response_set_cookie(response_t * self, const char * key, const char * value); */
+/* void response_redirect(response_t * self, const char * redirectUrl); */
+
+/* TEMPLATE.H */
+
+// 
+// native support for logicless text template engine like https://github.com/no1msd/mstch [C++]
+// partials support https://github.com/no1msd/mstch#partials
+// lambda-like support https://github.com/no1msd/mstch#lambdas
+/* const char * render_template(const char * templFileName, ?some_dict_type? template_data); */
 
 /* MAIN.C */
 typedef struct context_s {
@@ -26,6 +44,7 @@ typedef struct context_s {
 
 static response_t * handle_homepage(app_t * app, request_t * req, void * ctx);
 static response_t * handle_students(app_t * app, request_t * req, void * ctx);
+static response_t * handle_studentById(app_t * app, request_t * req, void * ctx);
 
 int main(void) {
 	context_t ctx = {
@@ -36,6 +55,7 @@ int main(void) {
 	app_setContext(app, &ctx);
 	app_on(app, "/", handle_homepage, (const char * []) {"GET", NULL});
 	app_on(app, "/students", handle_students, (const char * []) {"GET", "POST", NULL});
+	app_on(app, "/students/<int:student_id>", handle_studentById, (const char * []) {"GET", "POST", NULL});
 	app_run(app);
 	app_free(app);
 	return 0;
@@ -43,11 +63,18 @@ int main(void) {
 
 static response_t * handle_homepage(app_t * app, request_t * req, void * ctx) {
 	/**/
+	// template.html should be in /templates folder
+	// return response_new_render_template("template.html", template_data);  // template_data - ?
 	return response_new();
 }
 
 static response_t * handle_students(app_t * app, request_t * req, void * ctx) {
 	/**/
+	return response_new();
+}
+
+static response_t * handle_studentById(app_t * app, request_t * req, void * ctx) {
+	/* get student_id integer by name from req object */
 	return response_new();
 }
 
@@ -61,10 +88,18 @@ struct app_s {
 
 struct request_s {
 	/* smth */
+	/* GET URI args dict */
+	/* POST form dict */
+	/* headers dict */
+	/* files dict */
+	/* cookies dict */
 };
 
 struct response_s {
 	/* smth */
+	/* status code */
+	/* body data */
+	/* headers dict */
 };
 
 app_t * app_new(void) {
