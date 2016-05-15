2116644f2286efd271f2b30f8ef5015e6b0be804
PublicHadyniak <public.hadyniak@gmail.com>
2016-05-15 13:48:40 +0300

    Update tree.h

--- courses/prog_base_2/tasks/traversal/tree.h
+++ courses/prog_base_2/tasks/traversal/tree.h
@@ -6,10 +6,15 @@ typedef struct tree_s tree_t;
 
 typedef void (*traversal_callback_fn)(void * userData, void * nodeData);
 typedef void * (*data_constructor_fn)(void * fileTreeNode);
+typedef void (*data_destructor_fn)(void * nodeData);
 
-// parse stream data t build a tree
-tree_t * tree_new_from_stream(FILE * inputStream, data_constructor_fn callback);
-// tree destructor
+// open file and parse it's contents. Build a tree of students
+tree_t * tree_new_from_stream(
+		FILE * inputStream, 
+		data_constructor_fn constructorCallback, 
+		data_destructor_fn destructorCallback
+	);
+// destructor
 void tree_free(tree_t * self);
 // tree nodes traversal with callback function and userdata to pass to each callback call
 void tree_traversal(tree_t * self, void * userData, traversal_callback_fn callback);
