d1d6377a1e1e210e4df7bb036bd96ecbb83ebfbe
Ruslan Hadyniak <public.hadyniak@gmail.com>
2017-10-01 20:07:07 +0300

    Create ns_convert.c

---
+++ courses/prog_base/tasks/numeral_systems_conversion/ns_convert.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* ns_convert(char* out, const char * number, unsigned int sourceBase, unsigned int destBase);
+
+#define __LEN(X) sizeof(X)/sizeof(X[0])
+
+struct TestCase {
+    char * sourceNumber;
+    unsigned int sourceBase;
+    char * destNumber;
+    unsigned int destBase;
+};
+
+int compare(char * n1, char * n2) {
+    int i;
+    for (i = 0; n1[i] != '\0' && n1[i] != '.'; i++)
+        ;
+    if (n1[i] == '.') {
+        i += 2;  // accuracy of fraction
+    }
+    return 0 == strncmp(n1, n2, i);
+}
+
+int main()
+{
+    struct TestCase testCases[] = {
+        {"0", 15, "0", 27},
+        {"3", 10, "11", 2},
+        {"F", 16, "15", 10},
+        {"5.5", 10, "12.11111", 3},
+        {"125.125", 6, "311.03323", 4},
+        {"125.125", 6, "53.24537", 10},
+        {"53.24537", 10, "125.125", 6},
+        {"125.125", 6, "49.27765", 11},
+        {"125.125", 6, "32.42F89", 17},
+        {"125.125", 6, "1P.6OAAA", 28},
+        {"53.24537", 10, "125.125", 6},
+        {"10101.111", 10, "7SL.3ZUTD", 36},
+    };
+
+    int lenCases = __LEN(testCases);
+    int i;
+    char sourceNumber[1000];
+    char * res = NULL;
+    struct TestCase * tCase = NULL;
+
+    puts("ENCODE:\n");
+    for (i = 0; i < lenCases; i++) {
+        tCase = &testCases[i];
+		printf("%s(%i) = ", tCase->sourceNumber, tCase->sourceBase);
+		fflush(stdout);
+        res = ns_convert(sourceNumber, tCase->sourceNumber, tCase->sourceBase, tCase->destBase);
+		printf("{%s(%i)} == {%s} // %s\n", tCase->destNumber, tCase->destBase, res, compare(res, tCase->destNumber) ? "TRUE" : "FALSE" );
+		fflush(stdout);
+    }
+    return 0;
+}
